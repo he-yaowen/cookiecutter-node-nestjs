8de3217520597a7c1a76e7d4372911fb03cf147e
HE Yaowen <he.yaowen@hotmail.com>
2022-08-18 21:08:13 +0800

    Add configuration file for jest

---
+++ {{cookiecutter.project_slug}}/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+    moduleFileExtensions: ["js", "json", "ts"],
+    rootDir: ".",
+    testRegex: ".*\\.spec\\.ts$",
+    transform: {
+        "^.+\\.(t|j)s$": "ts-jest"
+    },
+    collectCoverageFrom: [
+        "**/*.(t|j)s"
+    ],
+    coverageDirectory: "../coverage",
+    testEnvironment: "node",
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1"
+    }
+};
