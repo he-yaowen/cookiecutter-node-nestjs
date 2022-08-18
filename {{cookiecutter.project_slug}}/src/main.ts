8af409d1b8deced546695291b55a77b49d554ad9
HE Yaowen <he.yaowen@hotmail.com>
2022-08-19 00:04:29 +0800

    Add support to generate API documents with swagger

--- {{cookiecutter.project_slug}}/src/main.ts
+++ {{cookiecutter.project_slug}}/src/main.ts
@@ -1,4 +1,5 @@
 import {NestFactory} from "@nestjs/core";
+{% if cookiecutter.use_swaggger == 'yes' %}import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";{% endif %}
 import {ApplicationModule} from "./modules/app.module";
 
 const DEFAULT_PORT = 3000;
@@ -6,7 +7,17 @@ const DEFAULT_PORT = 3000;
 async function bootstrap(): Promise<void>
 {
     const app = await NestFactory.create(ApplicationModule);
+    {% if cookiecutter.use_swaggger == 'yes' %}
+    const options = new DocumentBuilder()
+        .setTitle("API Template NestJS")
+        .setDescription("The NestJS API description")
+        .setVersion("1.0")
+        .build();
 
+    const document = SwaggerModule.createDocument(app, options);
+
+    SwaggerModule.setup("api", app, document);
+    {% endif %}
     await app.listen(process.env["PORT"] || DEFAULT_PORT);
 }
 
